1cac49dd209566458b3db03391d894b84d6f2060
Guillaume <xgold3nfir3@gmail.com>
2024-08-08 19:41:19 +0200

    translation in english and doc

--- CTkGif.py
+++ CTkGif.py
@@ -3,58 +3,71 @@ import customtkinter as ctk
 
 
 class CTkGif(ctk.CTkLabel):
-    """ Affichage d'un gif """
 
-    def __init__(self, master: any, path, loop=True, acceleration=1, repeat=1, **kwargs):
+    def __init__(self, master: any, file_path: str, loop: bool = True, acceleration: float = 1, repeat: int = 1,
+                 **kwargs) -> None:
+        """ CustomTkinter widget to display and manage a GIF file (as a succession of frames)
+        :param master: the parent widget
+        :param file_path: the path to the .gif file
+        :param loop: if the gif should start again when its end is reached
+        :param acceleration: factor of speed to play the gif animation
+        :param repeat: the number of times the gif should be played if not looped
+        :param kwargs: params for the parent CTkLabel if needed """
         super().__init__(master, **kwargs)
+
         if acceleration <= 0:
             raise ValueError('Acceleration must be strictly positive')
-        self.master = master  #  master
-        self.repeat = repeat  # Nombre repeat max
-        self.configure(text='')  # Retire texte base
-        self.path = path  # Chemin gif
-        self.count = 0  # Nombre d'animations réalisés
-        self.loop = loop  # tourne on a l'infini ?
-        self.acceleration = acceleration  # facteur de ralentissement
-        self.index = 0  # Frame affichée
-        self.is_playing = False  # état de l'affichage
-        self.gif = Image.open(path)  # Image ouverte
-        self.n_frame = self.gif.n_frames  # Nombres de frame de l'animation
-        self.frame_duration = self.gif.info['duration'] * 1/self.acceleration  # temps d'une frame
+
+        self.repeat = repeat
+        self.loop = loop
+        self.gif = Image.open(file_path)
+        self.n_frame = self.gif.n_frames
+        self.frame_duration = self.gif.info['duration'] * 1 / acceleration
+        self.configure(text='')
+
+        self.repeat_count = 0
+        self.frame_index = 0
+        self.is_playing = False
         self.force_stop = False
 
-    def update(self):  # Update l'affichage du gid
-        if self.index < self.n_frame:  # Si on est pas au bout de l'animation
-            if not self.force_stop:  # Si on est pas forcé de s'arrêter
-                self.gif.seek(self.index)  # Frame suivante
-                self.configure(image=ctk.CTkImage(self.gif, size=(300, 100)))  # Affichage
-                self.index += 1  # Indexation
+    def update(self):
+        """ Manage the next gif frame to display"""
+        if self.frame_index < self.n_frame:  # Before the last gif frame
+
+            if not self.force_stop:
+
+                self.gif.seek(self.frame_index)  # next frame
+                self.configure(image=ctk.CTkImage(self.gif, size=(300, 100)))  # update displayed image
+                self.frame_index += 1  # Indexation
                 self.after(int(self.frame_duration), self.update)  # Programmation prochaine frame
+
             else:
-                self.force_stop = False  # On passe en off
-        else:  # Si on est au bout
-            self.index = 0  # On revient au début
-            self.count += 1  # On incrémente le compteur
-            if self.is_playing and (self.count < self.repeat or self.loop):  # pas d'arrêt et on recommence
-                self.after(int(self.frame_duration), self.update)  # Programmation prochaine frame
+                self.force_stop = False
+
+        else:  # Last gif frame reached
+
+            self.frame_index = 0  # Next frame to be displayed is the first one
+            self.repeat_count += 1
+            if self.is_playing and (self.repeat_count < self.repeat or self.loop):
+                self.after(self.frame_duration, self.update)
             else:
-                self.is_playing = False  # On passe en off
+                self.is_playing = False
 
     def start(self):
-        """ débute l'animation si arrêtée"""
+        """ Launch the frame cycle"""
         if not self.is_playing:
-            self.count = 0
+            self.repeat_count = 0
             self.is_playing = True
             self.after(int(self.frame_duration), self.update)
 
     def stop(self, forced=False):
-        """arrête l'animation brusquement si forcé, à sa fin sinon"""
+        """ Stops instantaneously the frame cycle if forced, else ends the current cycle"""
         if self.is_playing:
             self.is_playing = False
             self.force_stop = forced
 
     def toggle(self, forced=False):
-        """ change le status de la lecture"""
+        """ toggle the cycle state, aka stops it if cycling, or starts it if stopped."""
         if self.is_playing:
             self.stop(forced=forced)
         else:
